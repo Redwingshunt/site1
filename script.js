74412ee25f19b7956948235e38cecf4cca14710c
Redwingshunt <alohaass12@gmail.com>
2025-06-02 16:35:19 +0530

    added new dark mode also updated the layout

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-// Smooth scrolling for internal links
+
+
+
+const themeToggle = document.getElementById('theme-toggle');
+
+themeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('light-theme');
+});
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
@@ -11,3 +19,21 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       }
     });
   });
+
+  document.addEventListener('mousedown', () => {
+    document.body.classList.add('hold-cursor');
+  });
+
+  document.addEventListener('mouseup', () => {
+    document.body.classList.remove('hold-cursor');
+  });
+
+  // Optional: for touch devices (long press)
+  document.addEventListener('touchstart', () => {
+    document.body.classList.add('hold-cursor');
+  });
+
+  document.addEventListener('touchend', () => {
+    document.body.classList.remove('hold-cursor');
+  });
+
